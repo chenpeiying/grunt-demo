ae247ec2d2b1c7c7be93aad9338da62620d8d681
chenpeiying <1099647011@qq.com>
2019-03-05 15:29:04 +0800

    v0.2

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    less: {
+      development: {
+        files: {
+                   'css/layout.css': 'less/layout.css'        
+        }              
+      }                  
+    }       
+  });
+    grunt.loadNpmTasks('grunt-contrib-less');
+      grunt.registerTask('default', ['less']);
+};
+
