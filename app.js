90adb90c49099ae321626f61896c7774b1abc6d7
chenpeiying <1099647011@qq.com>
2019-03-06 15:35:57 +0800

    sum

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 var x = process.argv[2] || 1,
-    y = process.argv[2] || 1;
+    y = process.argv[3] || 1;
 
-console.log('${x} + ${y} = ${Number(x)+Number(y)}');
+console.log(`${x} + ${y} = ${Number(x)+Number(y)}`);
 
 
 
