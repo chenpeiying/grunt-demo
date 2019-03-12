ff03c2c73f5664c72ffa6e8aff61d33409f63e81
chenpeiying <1099647011@qq.com>
2019-03-12 16:05:39 +0800

    删除js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,13 @@
 module.exports = function (grunt) {
-<<<<<<< HEAD
-   grunt.initConfig({
+  grunt.initConfig({
         cssmin: {  
               'rectangle.min.css': 'rectangle.css'
-            }        
+            }
+                  
   });
 
     grunt.loadNpmTasks('grunt-contrib-cssmin');
 
       grunt.registerTask('default', ['cssmin']); 
-=======
-  grunt.initConfig({
-    htmlmin: {
-      options: {
-                collapseWhitespace: true,
-          preserveLineBreaks: false
-          
-      },
-  files: {
-            src: './index.html',
-          dest: 'dist/index.html'
-          
-  }
-      
-    }
-               
-  });
-
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
-
-      grunt.registerTask('default', ['htmlmin']); 
->>>>>>> origin/cssmin
 
 };
