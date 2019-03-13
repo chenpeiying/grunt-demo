207c47294de338cb36bf55a4e95b99f251efdc48
chenpeiying <1099647011@qq.com>
2019-03-13 19:21:20 +0800

    打包合并

--- dist/bundle.js
+++ dist/bundle.js
@@ -65,3 +65,95 @@ function validate(field) {
 }
 });
 
+
+.rectangle{
+    width:370px;
+    margin: 50px auto;
+    font-size: 1.1em;
+}
+
+input, label, span {
+    margin: 5px 0;
+    font-size: 1em;
+    padding-left: 5px;
+
+}
+
+#rectangle-calc {
+    width: 70px;
+    margin-left: 63px;
+
+}
+
+span {
+    color: red;
+
+}
+
+#rectangle-width-validate,
+#rectangle-height-validate {
+    color: red;
+    margin-left: 63px;
+    font-size: 0.8em;
+
+}
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
+  <title>矩形计算器</title>
+  <link rel="stylesheet" href="rectangle.css">
+</head>
+<body>
+  <div class="rectangle">
+      <label>宽度：</label>
+      <input id="rectangle-width" type="text" data-label="宽度" autofocus>
+      <span>*</span>
+      <br>
+      <div id="rectangle-width-validate"></div>
+      <label>高度：</label>
+      <input id="rectangle-height" type="text" data-label="高度">
+      <span>*</span>
+      <br>
+      <div id="rectangle-height-validate"></div>
+      <input id="rectangle-calc" type="button" value="计算">
+      <br>
+      <label>周长：</label>
+      <input id="rectangle-perimeter" type="text" disabled>
+      <br>
+      <label>面积：</label>
+      <input id="rectangle-area" type="text" disabled>
+    </div>
+ <script src="rectangle.js"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
